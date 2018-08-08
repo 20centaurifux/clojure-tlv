ce89f0858f755b8bfd6dd8652b54a1d342b96cc8
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-08-08 18:50:11 +0200

    cljfmt plugin

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[lein-cljfmt "0.6.0"]])
